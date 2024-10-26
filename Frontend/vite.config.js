17a4bd8463b186c015391510f629f74516d1d71a
kaushal <kaushalkadariya2062@gmail.com>
2024-10-26 17:39:52 +0545

    updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    strictPort: true,
-    proxy: {
-      "/product/api": {
-        target: "https://tech-r.vercel.app/product/api",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+})
